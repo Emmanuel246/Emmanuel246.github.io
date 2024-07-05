dbefa3b53c6aadc503c66a1c5fb84f33f139e6d2
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-07-05 13:00:58 +0100

    okay

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-// const express = require('express');
-// const axios = require('axios');
-// const app = express();
-// const PORT = process.env.PORT || 8080;
-
-// app.get('/api/hello', async (req, res) => {
-//   const visitorName = req.query.visitor_name || 'Guest';
-
-//   // Get client IP address
-//   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-//   // Use an IP geolocation API to get location
-//   const locationResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
-//   const location = locationResponse.data.city || 'your city';
-
-//   // Use a weather API to get the current temperature
-//   const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=182ab95d87d5c82a1006ca1a31da03c1`);
-//   const temperature = weatherResponse.data.main.temp;
-
-//   res.json({
-//     client_ip: clientIp,
-//     location: location,
-//     greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
-//   });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -92,3 +58,45 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
+
+// const express = require("express");
+// const axios = require("axios");
+// const { lookup } = require("geoip-lite");
+
+// const app = express();
+
+// app.get("/api/hello", (req, res) => {
+//   let clientIp = (
+//     req.headers["x-forwarded-for"] ||
+//     req.connection.remoteAddress ||
+//     ""
+//   )
+//     .split(",")[0]
+//     .trim();
+//   clientIp = clientIp.split(":").pop();
+//   console.log(clientIp);
+//   const geo = lookup(clientIp);
+//   console.log(geo);
+//   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${geo.ll[0]}&lon=${geo.ll[1]}&units=metric&appid=182ab95d87d5c82a1006ca1a31da03c1`;
+
+//   axios({ url }).then(({ data }) => {
+//     console.log({ data });
+//     if (req.query.visitor_name) {
+//       return res.send({
+//         client_ip: clientIp,
+//         location: geo.city,
+//         greeting: `Hello, ${req.query.visitor_name}!, the temperature is ${data.main.temp} degrees Celsius in ${geo.city}`,
+//       });
+//     }
+//     return res.send({
+//       client_ip: clientIp,
+//       location: geo.city,
+//       greeting: `Hello, Visitor!, the temperature is ${data.main.temp} degrees Celsius in ${geo.city}`,
+//     });
+//   });
+// });
+
+// app.listen(4000, () => {
+//   console.log("Server is running on port 4000");
+// });
